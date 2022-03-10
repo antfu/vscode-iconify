eec9ff1c10e3122686c3e76cf6d76856f07ff23c
Marvin <272077995@qq.com>
2022-03-11 02:48:35 +0800

    feat: change COLLECTION_API to cdn (#20)

--- src/meta.ts
+++ src/meta.ts
@@ -2,4 +2,4 @@ export const EXT_NAMESPACE = 'iconify'
 export const EXT_ID = 'antfu.iconify'
 export const EXT_NAME = 'Iconify IntelliSense'
 
-export const COLLECTION_API = 'https://raw.githubusercontent.com/iconify/collections-json/master/json/'
+export const COLLECTION_API = 'https://cdn.jsdelivr.net/gh/iconify/icon-sets/json'
