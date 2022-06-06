54e3a06bc3c06c6567ad81e9a9848c8501207610
Small <smallp010@qq.com>
2022-06-06 11:10:23 +0800

    update jsdelivr
    cdn.jsdelivr.net is banned in China.

--- src/meta.ts
+++ src/meta.ts
@@ -2,4 +2,4 @@ export const EXT_NAMESPACE = 'iconify'
 export const EXT_ID = 'antfu.iconify'
 export const EXT_NAME = 'Iconify IntelliSense'
 
-export const COLLECTION_API = 'https://cdn.jsdelivr.net/gh/iconify/icon-sets/json'
+export const COLLECTION_API = 'https://fastly.jsdelivr.net/gh/iconify/icon-sets/json'
