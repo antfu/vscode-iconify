e5ccabe9eeb43c8392c28f0572f16d1ed0d4f6ec
Eureka <iherewithmyheart@gmail.com>
2022-08-04 21:54:31 +0800

    feat: configurable `cdnEntry` (#29)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/config.ts
+++ src/config.ts
@@ -41,6 +41,7 @@ export const config = reactive({
   excludes: createConfigRef<string[] | null>(`${EXT_NAMESPACE}.excludes`, null),
   fontSize: createConfigRef('editor.fontSize', 12),
   languageIds: createConfigRef(`${EXT_NAMESPACE}.languageIds`, []),
+  cdnEntry: createConfigRef(`${EXT_NAMESPACE}.cdnEntry`, 'https://cdn.jsdelivr.net/gh/iconify/icon-sets/json'),
 })
 
 export const enabledCollections = computed(() => {
