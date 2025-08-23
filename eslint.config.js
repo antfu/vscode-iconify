aa0e2802321d752f3fcc6682bf1be46c1a350765
Anthony Fu <github@antfu.me>
2025-08-23 23:06:46 +0900

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 // @ts-check
-const antfu = require('@antfu/eslint-config').default
+import antfu from '@antfu/eslint-config'
 
-module.exports = antfu(
-  {
-    ignores: [
-      'src/generated',
-    ],
-  },
-)
+export default antfu({
+  ignores: [
+    'src/generated',
+  ],
+})
